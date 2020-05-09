e770d7f780bfafb21a4319bfcf37afec7cb63dcb
tnagler <thomas.nagler@tum.de>
2020-05-09 15:17:46 +0200

    xauth

--- symlink-files.sh
+++ symlink-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare -a files=("init.vim" ".bashrc" ".functions.zsh" ".profile" 
                   ".valgrindrc" ".gitconfig" ".zshrc"
-                  ".R")
+                  ".R", ".vscode")
 
 ## now loop through the above array
 for file in "${files[@]}"
